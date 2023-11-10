2c7ccbae427761cc3e163dacc23a854030f6f738
lucasalvess96 <lucasalves.57@hotmail.com>
2023-11-10 19:27:38 -0300

    fix: alterado icones dos botões

--- src/app/features/welcome/welcome.module.ts
+++ src/app/features/welcome/welcome.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { WelcomeRoutingModule } from './welcome-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
-
+import { WelcomeRoutingModule } from './welcome-routing.module';
 
 @NgModule({
-  declarations: [
-    WelcomeComponent
-  ],
-  imports: [
-    CommonModule,
-    WelcomeRoutingModule
-  ]
+  declarations: [WelcomeComponent],
+  imports: [CommonModule, SharedModule, WelcomeRoutingModule],
 })
-export class WelcomeModule { }
+export class WelcomeModule {}
